c3be90e2780e86dc8e22743eb21cf39d0f1d5219
johnny <johnny1194@hotmail.com>
2021-03-27 23:23:14 -0400

    adding h2 tags html and appending first class in javascript

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-let h2 = document.createElement('h2')
-h2.textContent = "This site is the best"
-document.querySelector('body').appendChild(h2)
+
+const employmentSummary = document.createElement('div');
+employmentSummary.className = 'Employment Summary';
+employmentSummary.innerHTML = 
+`
+    <p>Amazon Robotics Associate </p>
+    <small>Amazon Warehouse Robbinsville NJ | September
+    2018 - present</small>
+    <ul>Supervision of floor health.</ul>
+    <ul>Reboot and control amazon robots.</ul>
+    <ul>Inventory restoration and updating lost articles.</ul>
+    <ul>Reports of possible items damage or lost.</ul>
+    <ul>Reports in inventory updates.</ul>
+    <p>Temporal Process Assitant(PG)<p>
+    <small>Amazon Warehouse Robbinsville NJ | May 2018 September
+    2018 - present<small>
+    <ul>Supervision of costumer orders</ul>
+    <ul>Analysis of floor productivity</ul>
+    <ul>Created detailed reports for manager</ul>
+`
+
+document.getElementsByClassName("block1")[0].appendChild(employmentSummary);
+
+
+
